cc8c37612132a25db8e46ccbedf3c56ce5b6ae54
GaoChengquan <529390053@qq.com>
2018-02-05 10:05:19 +0800

    ssm配置文件，数据库

---
+++ src/com/situ/mall/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.situ.mall.mapper;
+
+import com.situ.mall.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
